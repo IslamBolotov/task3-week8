d5df9ec0be11b54d9ee1ee17791ec41f6778d338
Islam Bolotov <islambolotov99@gmail.com>
2020-05-11 23:53:36 +0600

    second

--- js/poke.js
+++ js/poke.js
@@ -2,129 +2,120 @@ $(document).ready(function(){
 
 
     const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/?offset=` //`0&limit=20`
-    const idPoke = 'https://pokeapi.co/api/v2/pokemon/'
+    
     
 
     
     let index = 0;
-    $.ajax({
-        url:pokemonUrl+index+`&limit=20`,
-        success: function(data){
-            $('.previous').on('click',function(){
-                //$('body').html('')
-                index -= 20;
-                $.ajax({
-                    url:pokemonUrl+index+`&limit=20`,
-                    success: function(data){
-                        pokemon(data)
-                    },
-                    error: function(error){
-                        console.log(error);
-                    }
-                })
-            });
-            $('.next').on('click',function(){
-               // $('.container').html('')
-               
-                index += 20;
-                $.ajax({
-                    url:pokemonUrl+index+`&limit=20`,
-                    success: function(data){
-                       
-                        pokemon(data)
-                    },
-                    error: function(error){
-                        console.log(error);
-                    }
-                })
-            });
-
-            pokemon(data)
-        },
-        error: function(error){
-        console.log(error);
-        }
-    })
-
-
-
-
-    const pokemon = (data) =>{
+    const pokemon = async (index) =>{
         try{
+            $('ul').html('')
             $('.text-modal').html('')
-            $('.container').html('')
-            console.log(data);
-
-            //data = data.next;
-            const qwer = data.results
+            const response = await fetch(pokemonUrl+index+`&limit=20`)
+            const data  = await response.json()
             
-            qwer.forEach((item,i) => {
-                $('.container').append(`
-                    <div class="pokeName">${item.name}</div>
-                `)
-            })
+            await main(data)
             
-            
-            $('body').on('click','.pokeName',function(e){
-              // console.log($(this).text());
-                
-               let textt = ($(this).text());
-               
-               qwer.forEach(item =>{
-                   if(item.name === textt){
-                       console.log(item.name);
-                       id = item.url
-                       console.log(id);
-                       
-                   }
-               })
-            
-               
-                $.ajax({
-                    url: (id),
-                    success: function(result){
-                        console.log(result);
-                        $('.text-modal').append(`
-                        <div class="values">
-                            <h4>Имя: ${result.name}</h4>
-                            <h4>Тип: ${result.types[0].type.name}</h4>
-                            <h4>Рост: ${result.height}</h4>
-                            <h4>Вес: ${result.weight}</h4>
-                        </div>
-                        <div class="img">
-                            <img src=${result.sprites.front_default} width="200px">
-                        </div>
-
-                    `)
-                    $('.main-modal').fadeIn();
-                    
-                    },
-                    error: function(error){
-                        console.log(error);
-                        
-                    }
-
-                })
-              
-            })
-            
-
-            $('.close-modal').on('click',function() { 
-              
-                 $('.main-modal').fadeOut()
-            });
-      
         }
         catch(error){
             console.log(error);
             
         }
     }
+    pokemon(index)
+    
 
-//https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20
-//https://pokeapi.co/api/v2/pokemon/
+    $('body').on('click','.previous', async () => {
+        if(index == 0) return
+        index-=20
+        // $('.container').html('')
+        // const response = await fetch(pokemonUrl+index+`&limit=20`)
+        // const data  = await response.json()
+        
+        await pokemon(index);
+        
+        
+        
+    });
+    $('body').on('click','.next', async () => {
+        index+=20
+        // $('.container').html('')
+        // $('.text-modal').html('')
+        // const response = await fetch(pokemonUrl+index+`&limit=20`)
+        // const data  = await response.json()
+        
+        await pokemon(index);
+        
+    });
+
+    $('.close-modal').on('click',function() { 
+        $('.text-modal').html('')
+         $('.main-modal').fadeOut()
+      
+    });
 
 
+    function main(data) {
+     
+            
+        $('.text-modal').html('')
+        $('ul').html('')
+        console.log(data);
+        
+        const qwer = data.results
+        
+        qwer.forEach((item,i) => {
+            $('ul').append(`
+            <li class="pokeName">${item.name}</li>
+            `)
 
+        })
+        $('ul').append(`
+            <button class="previous">Previous</button>   
+            <button class="next">Next</button>
+            `)
+        
+        $('.pokeName').on('click',function(){
+            
+            
+        let textt = ($(this).text());
+        
+        console.log($(this).index());
+        qwer.forEach(item =>{
+                if(item.name === textt){
+                    
+                    $.ajax({
+                        url: (item.url),
+                        success: function(result){
+                            //console.log(result);
+                            $('.text-modal').append(`
+                            <div class="values">
+                                <h4>Имя: ${result.name}</h4>
+                                <h4>Тип: ${result.types[0].type.name}</h4>
+                                <h4>Рост: ${result.height}</h4>
+                                <h4>Вес: ${result.weight}</h4>
+                            </div>
+                            <div class="img">
+                                <img src=${result.sprites.front_default} width="200px">
+                            </div>
 
+                        `)
+                            
+                        $('.main-modal').fadeIn();
+                        
+                        },
+                        error: function(error){
+                            console.log(error);
+                            
+                        }
+                        
+                    })
+                
+                }
+                
+            })
+        
+        })
+    
+    }
 })
